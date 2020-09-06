a78aa29accbc755ec3ee9fb8e2fbf0cf2a8bad06
EBEREGIT <samsoneberenjoku@gmail.com>
2020-09-06 20:15:22 +0100

    starter project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+// body parser configuration
+app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response, next) => {
+  response.json({ message: "Hey! This is your server response!" });
+  next();
+});
+
+
+module.exports = app;
