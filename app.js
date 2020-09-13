5fb9d90322c40be31d84a8c845b9f09da8024623
NJOKU SAMSON EBERE <samsoneberenjoku@gmail.com>
2020-09-13 12:35:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-require('dotenv').config()
 
 // body parser configuration
 app.use(bodyParser.json());
