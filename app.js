bea6e423df3f3776ff6496980f3d94abd38e351d
EBEREGIT <samsoneberenjoku@gmail.com>
2020-09-20 13:55:09 +0100

    register route completed

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-// require database connection 
-const dbConnect = require("./db/dbConnect");
+const bcrypt = require("bcrypt");
 
-// execute database connection 
+// require database connection
+const dbConnect = require("./db/dbConnect");
+const User = require("./db/userModel");
+
+// execute database connection
 dbConnect();
 
 // body parser configuration
@@ -14,6 +17,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
   next();
+});
+
+// register endpoint
+app.post("/register", (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch erroe if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
 });
 
 module.exports = app;
