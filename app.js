c8e201a4bc78dab851fc0c648ee4704deded439a
EBEREGIT <samsoneberenjoku@gmail.com>
2021-01-17 19:28:06 +0100

    CORS error handled

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ const auth = require("./auth");
 // execute database connection
 dbConnect();
 
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 // body parser configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
